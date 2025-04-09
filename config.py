3d10adb510403facf3b010e6dedeebf8d3a314cd
PerronBr826 <PerronBr826@northwested.org>
2025-04-09 10:12:32 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,18 @@
+# Pygame Configuration Module
+
+# Color constants (RGB)
+WHITE = (200, 200, 200)
+BLACK = (28, 28, 28)
+RED = (255, 100, 100)
+GREEN = (100, 255, 100)
+BLUE = (100, 100, 255)
+
+# Game window dimensions
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+# Game window title
+TITLE = "Pygame Template"
+
+# Frame rate (frames per second)
+FPS = 60
